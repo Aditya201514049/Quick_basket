78ab1e6572e00619c68955478bac6b40101623af
Aditya201514049 <adityasingha084@gmail.com>
2026-03-05 04:48:45 +0600

    controller function for register is added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+const authService = require('../services/auth.service');
+
+
+const register = async(req, res) =>{
+    try{
+
+        const {name, email, password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success: false,
+                mesagge: "Please provide name, email and password"
+            });
+        }
+
+        const user = await authService.registerUser({name, email, password});
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: {user}
+        });
+
+
+    }
+    catch(error){
+        res.status(400).json({
+            success: true,
+            message: error.message
+        });
+
+    }
+};
+
+module.exports = {
+    register
+}
