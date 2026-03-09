8e80083e05fe4dfa48deecfe66fdc89745329bf7
Aditya201514049 <adityasingha084@gmail.com>
2026-03-10 01:16:59 +0600

    public and private routes added, middleware added for authorized work

--- backend/src/middlewares/auth.middeware.js
+++ backend/src/middlewares/auth.middeware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
 const validateRegister = (req, res, next) => {
     const { name, email, password } = req.body;
     
@@ -44,7 +47,52 @@ const validateLogin = (req, res, next) => {
     next();
 };
 
+const authenticate = async(req, res, next) =>{
+    try{
+        const token = req.header('Authorization')?.replace('Bearer ','');
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Access denied Login required"
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId).select('-password');
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token"
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch(error){
+        res.status(401).json({
+            success: false,
+            message: 'Invalid token'
+        });
+    }
+};
+
+const authorize = (...roles) =>{
+    return (req, res, next) =>{
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({
+                success: false,
+                message: "Access denied, Insufficient permissions"
+            });
+        }
+        next();
+    };
+};
+
 module.exports = {
     validateRegister,
-    validateLogin
+    validateLogin,
+    authenticate,
+    authorize
 };
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product.controller');
+const { authenticate, authorize } = require('../middlewares/auth.middeware');
 
-router.post('/', productController.createProduct);
+// Public routes (no login needed)
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+
+// Protected routes (login + role required)
+router.post('/', authenticate, authorize('seller', 'admin'), productController.createProduct);
+router.put('/:id', authenticate, authorize('seller', 'admin'), productController.updateProduct);
+router.delete('/:id', authenticate, authorize('seller', 'admin'), productController.deleteProduct);
 
 module.exports = router;
